8877422a38355ff41253e2bd5c4f44c86a434db3
João <ut7vitor60@gmail.com>
2024-12-23 17:31:53 -0300

    Simplificando com funções e métodos

--- script.js
+++ script.js
@@ -5,20 +5,21 @@ const bt_descanso_longo = document.querySelector(".app__card-button--longo");
 
 const imagem = document.querySelector('.app__image'); 
 
+function alterarContexto(contexto){
+    html.setAttribute("data-contexto",contexto)
+    imagem.setAttribute("src",`./imagens/${contexto}.png`)
+}
 
 focoBt.addEventListener('click', () => {
-    html.setAttribute("data-contexto","foco");
-    imagem.src = "./imagens/foco.png"
+    alterarContexto("foco");
     //Aqui também posso utilizar o setAttribute. EX: minhaImagem.setAttribute("src", "caminho/da/nova/imagem.jpg");
     
 })
 
 bt_descanso_curto.addEventListener('click', () => {
-    html.setAttribute("data-contexto","descanso-curto");
-    imagem.src = "./imagens/descanso-curto.png"
+    alterarContexto("descanso-curto");
 })
 
 bt_descanso_longo.addEventListener('click', () => {
-    html.setAttribute("data-contexto","descanso-longo");
-    imagem.src = "./imagens/descanso-longo.png"
+    alterarContexto("descanso-longo"); 
 })
