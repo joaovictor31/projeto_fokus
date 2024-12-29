782b330eab7e09dd9f98e74fe2ee22321f3be4b7
João <ut7vitor60@gmail.com>
2024-12-28 21:24:59 -0300

    Alterando o foco do botão ao ser clicado

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const focoBt = document.querySelector(".app__card-button--foco");
 const bt_descanso_curto = document.querySelector(".app__card-button--curto");
 const bt_descanso_longo = document.querySelector(".app__card-button--longo");
 
+const botoes = document.querySelectorAll('.app__card-button');
+
+const musicaFocoInput = document.querySelector('#alternar-musica')
 
 //Mudar Imagem
 const imagem = document.querySelector('.app__image'); 
@@ -12,6 +15,12 @@ const titulo = document.querySelector(".app__title");
 
 
 function alterarContexto(contexto) {
+    botoes.forEach(function(contexto){
+            contexto.classList.remove('active')
+        })
+
+        /* Aqui, está removendo a classe active de todos os botões, garantindo que nenhum deles permaneça com a aparência de "ativo". No entanto, o botão clicado é marcado como "ativo" novamente após essa operação. */
+        
     html.setAttribute('data-contexto', contexto)
     imagem.setAttribute('src', `/imagens/${contexto}.png`)
     switch (contexto) {
@@ -37,15 +46,17 @@ function alterarContexto(contexto) {
 
 focoBt.addEventListener('click', () => {
     alterarContexto("foco");
+    focoBt.classList.add('active')
 })
 
 bt_descanso_curto.addEventListener('click', () => {
     alterarContexto("descanso-curto");
-
+    bt_descanso_curto.classList.add('active')
 })
 
 bt_descanso_longo.addEventListener('click', () => {
     alterarContexto("descanso-longo"); 
+    bt_descanso_longo.classList.add('active')
 
 })
 
